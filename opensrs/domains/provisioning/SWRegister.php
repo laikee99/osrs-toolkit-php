c726fd0beb8eb220e517c399c6c6d9d859f68585
Laike Du <dwtfromchina@gmail.com>
2025-11-21 01:25:06 +0800

    Implement dynamic required fields and validation
    Added dynamic required fields based on registration type and validation for attributes.

--- opensrs/domains/provisioning/SWRegister.php
+++ opensrs/domains/provisioning/SWRegister.php
@@ -3,6 +3,7 @@
 namespace opensrs\domains\provisioning;
 
 use opensrs\Base;
+use opensrs\Exception;
 
 class SWRegister extends Base
 {
@@ -15,8 +16,9 @@ class SWRegister extends Base
     public $resultFullFormatted;
     public $resultFormatted;
 
-    public $requiredFields = array(
-        'attributes' => array(
+    // Dynamic required fields per reg_type
+    public $requiredFieldsByType = array(
+        'new' => array(
             'domain',
             'custom_nameservers',
             'custom_tech_contact',
@@ -24,8 +26,28 @@ class SWRegister extends Base
             'reg_username',
             'reg_password',
             'reg_type',
-            ),
-        );
+        ),
+        'transfer' => array(
+            'domain',
+            'auth_info',
+            'reg_username',
+            'reg_password',
+            'reg_type',
+            //'custom_transfer_nameservers' //is optional in API, enforced as domains existing if not set
+        ),
+        'assign' => array(
+            'domain',
+            'reg_username',
+            'reg_password',
+            'reg_type',
+        ),
+        'owner_change' => array(
+            'domain',
+            'reg_username',
+            'reg_password',
+            'reg_type',
+        ),
+    );
 
     public function __construct($formatString, $dataObject, $returnFullResponse = true)
     {
@@ -35,6 +57,8 @@ class SWRegister extends Base
 
         $this->_validateObject($dataObject);
 
+        $this->validateAttributes((array)$dataObject->attributes);
+
         $this->send($dataObject, $returnFullResponse);
     }
 
@@ -43,13 +67,49 @@ class SWRegister extends Base
         parent::__destruct();
     }
 
+    private function validateAttributes($attributes)
+    {
+        if (!isset($attributes['reg_type'])) {
+            throw new Exception('SWRegister Error - Missing required field: reg_type');
+        }
+
+        $regType = strtolower($attributes['reg_type']);
+
+        if (!isset($this->requiredFieldsByType[$regType])) {
+            throw new Exception("SWRegister Error - Unsupported reg_type: {$regType}");
+        }
+
+        foreach ($this->requiredFieldsByType[$regType] as $field) {
+            if (!isset($attributes[$field])) {
+                throw new Exception("SWRegister Error - Missing required field for '{$regType}': {$field}");
+            }
+        }
+
+        // Special handling for transfer - ensure custom_transfer_nameservers is always present as an array
+        if ($regType === 'transfer') {
+            if (!isset($attributes['custom_transfer_nameservers'])) {
+                $attributes['custom_transfer_nameservers'] = 0;
+            }
+        }
+
+        // Example for .EU domain validation
+        if (isset($attributes['domain']) && preg_match('/\.eu$/i', $attributes['domain'])) {
+            if (empty($attributes['lang_pref'])) {
+                throw new Exception("SWRegister Error - .EU domains require 'lang_pref'.");
+            }
+            if (empty($attributes['eu_country_of_residence']) && empty($attributes['eu_country_of_citizenship'])) {
+                throw new Exception("SWRegister Error - .EU domains require 'eu_country_of_residence' or 'eu_country_of_citizenship'.");
+            }
+        }
+
+        // You can extend here for other TLDs like .CA or .DE
+    }
+
     public function customResponseHandling($arrayResult, $returnFullResponse = true)
     {
-        /* Added by BC : NG : 16-7-2014 : To set error message for Insufficient Funds */
-        if (isset($arrayResult['attributes']['forced_pending']) and $arrayResult['attributes']['forced_pending'] != '' and $arrayResult['is_success'] == 1) {
+        if (isset($arrayResult['attributes']['forced_pending']) && $arrayResult['attributes']['forced_pending'] != '' && $arrayResult['is_success'] == 1) {
             $arrayResult['is_success'] = 0;
 
-            // Get Resonse Text 'Registration successful'  when insufficient fund
             if ($arrayResult['response_text'] == 'Registration successful') {
                 $arrayResult['response_text'] = 'Insufficient Funds';
             }
